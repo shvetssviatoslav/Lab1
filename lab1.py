829e17699aaf9d657131ad376300bdc57f0346eb
shvetssviatoslav <shvets.sviatik@gmail.com>
2025-09-10 09:37:11 +0300

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -5,7 +5,27 @@ country = 'USA'
 student_die = False
 fofan_is_good = True
 age_people = [5, 20, 3]
-hights_baobab =  (6, 22)
-bobofan = { 22, 45}
-nig={"name","fofan","age"}
-print (age,high,name,country,student_die,fofan_is_good,age_people,hights_baobab,bobofan,nig)
+hights_baobab = (6, 22)
+bobofan = {22, 45}
+nig = {"name", "fofan", "age"}
+
+print(type(age))
+print(age)
+print(type(high))
+print(high)
+print(type(name))
+print(name)
+print(type(country))
+print(country)
+print(type(student_die))
+print(student_die)
+print(type(fofan_is_good))
+print(fofan_is_good)
+print(type(age_people))
+print(age_people)
+print(type(hights_baobab))
+print(hights_baobab)
+print(type(bobofan))
+print(bobofan)
+print(type(nig))
+print(nig)
